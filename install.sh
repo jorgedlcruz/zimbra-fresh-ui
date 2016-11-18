1160e46bf52ab48abd67faaab7085e502a24c7a3
jorgedlcruz <jorgedlcruz@gmail.com>
2016-11-18 13:03:11 +0000

    Simple installer
    Added a simple installer which take care of the Backup and so on

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+## Backup of the files, in case something goes wrong
+tar -zcvf skins.tar.gz /opt/zimbra/jetty/webapps/zimbra/skins
+tar -zcvf css.tar.gz /opt/zimbra/jetty/webapps/zimbra/css
+
+## Rsync of the new files into the actual folders
+rsync -a zimbra-fresh-ui/skins/ /opt/zimbra/jetty/webapps/zimbra/skins/
+rsync -a zimbra-fresh-ui/css/ /opt/zimbra/jetty/webapps/zimbra/css/
+
+## Mailboxd restart, it can take some time
+su - zimbra -c 'zmmailboxdctl restart'
+
+## Browser reload
+echo "Congratulations, you can experience now the changes, please refresh your browser"
