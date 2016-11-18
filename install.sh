60c57e982b31d0b85da8aeb1b85299009c66cde4
jorgedlcruz <jorgedlcruz@gmail.com>
2016-11-18 15:19:16 +0000

    Support for 8.7.1

--- install.sh
+++ install.sh
@@ -12,7 +12,15 @@ fi
 ## Rsync of the new files into the actual folders
 echo "Doing a rsync of the new files into the Zimbra folders"
 rsync -a skins/ /opt/zimbra/jetty/webapps/zimbra/skins/
-rsync -a css/ /opt/zimbra/jetty/webapps/zimbra/css/
+
+echo "Are you running ZCS 8.7.1?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) rsync -a 8.7.1/css/ /opt/zimbra/jetty/webapps/zimbra/css/;;
+        No ) rsync -a 8.6/8.7/css/ /opt/zimbra/jetty/webapps/zimbra/css/;;
+    esac
+done
+
 
 ## Mailboxd restart, it can take some time
 echo "Restarting the Mailboxd"
